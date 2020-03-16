c18b99ddd53fbd12f2696d60601723b55245be21
katiekirchner <katiexkirchner@gmail.com>
2020-03-16 15:11:45 -0700

    move package.json, rest of server build

--- risk-attack-2-0/app.js
+++ risk-attack-2-0/app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var testsRouter = require ('./tests/test');
+
 
 if(process.env.NODE_ENV === 'development') {
   require("dotenv").config();
@@ -25,6 +27,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/tests', testsRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- risk-attack-2-0/db/index.js
+++ risk-attack-2-0/db/index.js
@@ -1,4 +1,4 @@
 const pgp = require('pg-promise')();
-const connection = pgp(process.env.DATABASE_URL);
+const connection = pgp(process.env.DATABASE_URL || "postgres://katiekirchner@localhost:5432/risk_db");
 
 module.exports = connection;
