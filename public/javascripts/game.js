1bb02cb07d9487d5156afe17f5dfcde367ecf247
Daniel <dbelmeur@mail.sfsu.edu>
2020-05-03 02:27:43 -0700

    api work

---
+++ public/javascripts/game.js
@@ -0,0 +1,23 @@
+console.log('the script is running!');
+
+const moveTestElement = document.querySelector('#move-test');
+
+moveTestElement.addEventListener('click', event => {
+    event.preventDefault();
+
+    const coordinate = document.querySelector('#move-coordinate').value;
+    const uri = moveTestElement.attributes.href.value;
+
+    console.log(coordinate, uri)
+
+    fetch(uri,
+        { method: 'post',
+            body: JSON.stringify({ coordinate }),
+            headers: {
+            'Content-Type': 'application/json'
+            }
+        })
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.log(error))
+});
--- routes/api/game.js
+++ routes/api/game.js
@@ -8,4 +8,12 @@ router.post('/:id', function(request, response) {
     response.json({testMessage: `Posted to ${id}`})
 });
 
+router.post('/:id/move', function(request, response) {
+    //response.send('respond with a resource');
+    const { id } = request.params;
+    const { coordinate } = request.body;
+    console.log('the id is ' , id);
+    response.json({testMessage: `Posted to id ${id}/move: ${coordinate}`})
+});
+
 module.exports = router;
