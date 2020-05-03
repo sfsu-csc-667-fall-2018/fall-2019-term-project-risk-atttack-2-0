3921fe8723bd34b80bfc3fbe2726ffc11f7b3eac
Daniel <dbelmeur@mail.sfsu.edu>
2020-05-02 18:48:22 -0700

    routes to login and register up
    for some reason pug wont display but jade files will, so im not messing with that yet
    
    also the background image for the login/register pages wont load with jade

--- routes/pages/index.js
+++ routes/pages/index.js
@@ -7,7 +7,7 @@ router.get('/', function(req, res, next) {
     res.render('index', { title: 'Express' });
 });
 
-router.get('/users/reguster', (_, res) => {
+router.get('/users/register', (_, res) => {
     res.render('unauthenticated/register')
 });
 
--- routes/users.js
+++ routes/users.js
@@ -3,8 +3,13 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/login', function(_, response) {
-  response.send('respond with a resource');
+  //response.send('respond with a resource');
   response.render('unauthenticated/login')
+});
+
+router.get('/register', function(_, response) {
+  //response.send('respond with a resource');
+  response.render('unauthenticated/register')
 });
 
 module.exports = router;
