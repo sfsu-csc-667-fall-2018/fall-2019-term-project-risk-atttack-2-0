5d7fe361ad5e11edd78ad70c7014a17ffe411fd0
katiekirchner <katiexkirchner@gmail.com>
2020-05-08 21:08:25 -0700

    style pages

--- db/index.js
+++ db/index.js
@@ -1,4 +1,15 @@
 const pgp = require('pg-promise')();
 const connection = pgp(process.env.DATABASE_URL || "postgres://katiekirchner@localhost:5432/risk_db");
 
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const port = 3000
+
+app.get('/', (request, response) => {
+  response.json({ info: 'Node.js, Express, and Postgres API' })
+})
+
+
+
 module.exports = connection;
