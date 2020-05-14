4f76c28e78d82af1a8c1337cf3135b68b95fe5e0
Daniel <dbelmeur@mail.sfsu.edu>
2020-05-13 18:34:45 -0700

    Migrations work!!!

--- config/config.js
+++ config/config.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 module.exports = {
   "development": {
     "username": "postgres",
+    "password": "password",
     "use_env_variable": "DATABASE_URL",
     "dialect": "postgres"
   },
--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,5 @@
 const pgp = require('pg-promise');
 
-const connection = pgp(process.env.DATABASE_URL|| "postgres://Daniel@localhost:5432/risk_db");
+const connection = pgp(process.env.DATABASE_URL|| "postgres://postgres@localhost:5432/risk_db");
 
 module.exports = connection;
