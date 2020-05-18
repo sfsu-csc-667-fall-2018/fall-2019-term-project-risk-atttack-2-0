65d7f669a2183d97a826a912d04109fb76b6b643
Belmeurrr <47770901+Belmeurrr@users.noreply.github.com>
2020-05-18 16:08:40 -0700

    Revert "grabbing more of katies work"

--- db/users.js
+++ db/users.js
@@ -4,25 +4,21 @@ const create = (email, password, username) =>
   db.any("INSERT INTO users (email, password, username) VALUES ($1, $2, $3) RETURNING id, email, username",  [email, password, username] );
 
 const findById = (id) =>
-      db.one("SELECT (id, email, createdat) FROM users WHERE id=${id}", {id} );
+      db.one("SELECT (id, email, createdAt) FROM users WHERE id=${id}", {id} );
 
 const findByEmailAndPassword = (email, password) =>
-  db.one("SELECT id, email, createdat FROM users WHERE email=${email} and password=${password}", { email, password });
+  db.one("SELECT id, email, createdAt FROM users WHERE email=${email} and password=${password}", { email, password });
 
 const findByUsernameAndPassword = (username, password) =>
-    db.one("SELECT id, username, createdat FROM users WHERE username=${username} and password=${password}", { username, password });
+    db.one("SELECT id, username, createdAt FROM users WHERE username=${username} and password=${password}", { username, password });
 
 const getHash = (username) =>
-    db.one("SELECT id, createdat, password FROM users WHERE username=${username}", {username} );
-
-const findByName = (player1, player2, player3, player4) =>
-    db.any("SELECT id, username FROM users WHERE (username=${player1}) or (username=${player2}) or (username=${player3}) or (username=${player4}) ", {player1, player2, player3, player4} );
+    db.one("SELECT id, createdAt, password FROM users WHERE username=${username}", {username} );
 
 module.exports = {
   create,
   findByEmailAndPassword,
   findById,
-  findByName,
   findByUsernameAndPassword,
   getHash
 };
--- socket/index.js
+++ socket/index.js
@@ -19,3 +19,4 @@ const init = (app, server) => {
 };
 
 module.exports = {init};
+
